95088514f45a44fb70ae816161a6d18fc0cf6b91
Tien Tran <ttran@genixventures.com>
2017-12-15 13:17:04 +1100

    Host webdav file on the root

--- webdav/run.sh
+++ webdav/run.sh
@@ -5,6 +5,6 @@ cp /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 htpasswd -cb /etc/apache2/webdav.password $USERNAME $PASSWORD
 chown root:www-data /etc/apache2/webdav.password
 chmod 640 /etc/apache2/webdav.password
-sed -i 's/Listen 80/Listen 8080/g' /etc/apache2/ports.conf
+sed -i 's/Listen 80$/Listen 8080/g' /etc/apache2/ports.conf
 
 su -m -s /bin/bash -c '/usr/sbin/apache2 -D FOREGROUND' www-data
