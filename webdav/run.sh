4636a0da030ec4d6d8bf756ecb95a2dcdb869736
Tien Tran <ttran@genixventures.com>
2018-01-23 20:57:47 +1100

    Append admin password if password file exists

--- webdav/run.sh
+++ webdav/run.sh
@@ -2,7 +2,12 @@
 
 cp /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-htpasswd -cb /etc/apache2/webdav.password $USERNAME $PASSWORD
+if [[ -f /etc/apache2/webdav.password ]]; then
+  htpasswd -b /etc/apache2/webdav.password $USERNAME $PASSWORD
+else
+  htpasswd -cb /etc/apache2/webdav.password $USERNAME $PASSWORD
+fi
+
 chown root:www-data /etc/apache2/webdav.password
 chmod 640 /etc/apache2/webdav.password
 sed -i 's/Listen 80$/Listen 8080/g' /etc/apache2/ports.conf
